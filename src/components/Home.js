14bce1d1c371b19e4ab177efdf619366b18a3a0e
Huey <jasperjeng@gmail.com>
2017-12-30 20:40:16 -0800

    first pargraph of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import "../styles/Home.css";
+
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        Home
+      <div className="home-content">
+        <h1 className="paragraph-header">Who am I</h1>
+        <p>
+        Hey there! My name's Jasper Jeng. I come from the Bay Area (San Jose). I'm an avid fan of emerging new (or old) things in general. 
+        So things like machine learning, faster browsers (Mozilla), electric cars (Tesla) are all super fun to look at.       
+        </p>
+        <hr />
       </div>
     );
   }
