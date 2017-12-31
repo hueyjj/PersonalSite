9179635b5b84e8935a46d307fcf680d4b99516b5
Huey <jasperjeng@gmail.com>
2017-12-30 18:05:58 -0800

    Added Github and LinkedIn external links

---
+++ src/components/LinkedIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LinkedIn extends React.Component {
+  render() {
+    return (
+      <div>
+        LinkedIn
+      </div>
+    );
+  }
+}
+
+export default LinkedIn;
