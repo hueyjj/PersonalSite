13fba1deed1700eec1287df26d40e819db0a9865
Huey <jasperjeng@gmail.com>
2017-12-30 21:17:52 -0800

    added some intro content

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,25 @@ import "../styles/Home.css";
 class Home extends React.Component {
   render() {
     return (
-      <div className="home-content">
-        <h1 className="paragraph-header">Who am I</h1>
+      <div className="content-home">
         <p>
-        Hey there! My name's Jasper Jeng. I come from the Bay Area (San Jose). I'm an avid fan of emerging new (or old) things in general. 
-        So things like machine learning, faster browsers (Mozilla), electric cars (Tesla) are all super fun to look at.       
+          Hey there! My name's <mark className="mark-blue">Jasper Jeng</mark>. I come from the Bay Area (San Jose). I'm an avid fan of emerging new (or old) things in general.
+          So things like machine learning, faster browsers (Mozilla), electric cars (Tesla) are all super fun to keep up with.
+
+          <br /><br />
+
+          Lately there's been stuff like Go, Rust, Web Assembly that I've been hearing more and more. Hopefully I can make enough time to dive into them.
+
+          <br /><br />
+
+          Anyway... I'm an aspiring <mark className="mark-orange">software engineer/developer/ ? </mark> hoping to add a <mark className="mark-red">twist</mark> somehow...
+
+          <br /><br />
+          But for the time being I'll be hanging around at <mark className="mark-darkblue">UCSC</mark>
+          <br /><br />
+
+          As always good luck and have fun!
+
         </p>
         <hr />
       </div>
